01272326d4060f6c33cdb29c912202a83d0d63cb
Darshil-Chodvadiya <darshilchodvadiya59@gmail.com>
2023-12-26 09:14:14 +0530

    bootstrap template integration

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Wrapper from './Wrapper';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Sidebar/>
+    <Wrapper/>
+    <Footer/>
+    </>
+   
+  );
+}
+
+export default App;
